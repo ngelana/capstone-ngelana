f92ca978a51504ab516e1397c822adf72aa1de75
anggerbudi <anggerbudi9@gmail.com>
2024-06-19 22:04:19 +0700

    update(ml-backend): last update for now. moving to ml branch for testing

--- cloud-computing/fastAPI/main.py
+++ cloud-computing/fastAPI/main.py
@@ -1,12 +1,9 @@
 import orjson
 import uvicorn
-import numpy as np
 
 from typing import Any
-from pydantic import BaseModel
 from contextlib import asynccontextmanager
-from similar_places_data import reviews_data, X
-from sklearn.metrics.pairwise import cosine_similarity
+
 from fastapi import FastAPI, Response, HTTPException
 from fastapi.encoders import jsonable_encoder
 
@@ -15,7 +12,7 @@ from fastapi_cache.backends.inmemory import InMemoryBackend
 from fastapi_cache.decorator import cache
 
 
-class ORJsonCoder(Coder):
+class CustomCoder(Coder):
     @classmethod
     def encode(cls, value: Any) -> bytes:
         return orjson.dumps(
@@ -28,52 +25,23 @@ class ORJsonCoder(Coder):
         return orjson.loads(value)
 
 
-class UserPreferences(BaseModel):
-    input: str
-    types: list
-
-
 @asynccontextmanager
 async def lifespan(api: FastAPI):
     FastAPICache.init(InMemoryBackend())
     yield
     await FastAPICache.clear()
 
+
 app = FastAPI(lifespan=lifespan)
 
 
-# For similar place recommendation
-# Example: ip:port/similar-places/ChIJwZQeZ7vZwokR5jX6v7JZ3Zg
-# Return : Response JSON body with 10 similar places
 @app.get("/similar-places/{place_id}")
-@cache(namespace="similar-places", expire=300)
+@cache(namespace="similar-places", coder=CustomCoder, expire=300)
 async def content_based(place_id: str):
     try:
-        place_idx = reviews_data[reviews_data['id'] == place_id].index[0]
-        place_review = X['review'][place_idx]
-        place_types = X['types'][place_idx]
-        place_vector = np.concatenate([place_review, [place_types]])
-        all_vectors = np.hstack([X['review'], X['types'].reshape(-1, 1)])
-        similarities = cosine_similarity([place_vector], all_vectors)[0]
-        similar_indices = np.argsort(similarities)[-10:][::-1]
-        similar_places = reviews_data['id'].iloc[similar_indices].to_string(index=False).split()
-        return similar_places
-
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Place ID not found")
+        return print("hello")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
-
-
-@app.post("/places-generate/")
-async def generate_places(user_preferences: UserPreferences):
-    try:
-        print("hello")
-        test = user_preferences.input
-        result_json = orjson.dumps(test)
-        return Response(content=result_json, media_type="application/json")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == '__main__':
